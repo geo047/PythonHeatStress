b42050e7ddb4e961bd24b87c36759c354073a0c6
geo047 <geo047@gmail.com>
2023-02-24 17:07:41 +1000

    24/02 Added R functionality via rpy2. Pandas dataframe being passed into R function.

--- lib/heatstress.py
+++ lib/heatstress.py
@@ -27,7 +27,7 @@ mydf['treatment'].replace({'trt_1':'diet I', 'trt_2':'diet II','trt_3':'diet III
 # remove rows with these dates 2022-09-22, and after 17/10
 mydf.query('~(Sample_Date=="2022-09-22" or Sample_Date > "2022-10-17") ', inplace=True)
 mydf.Sample -= 1 # reindexing Sample day number to start from 1.
-mydf = mydf.astype({"Sample": 'category'})
+#mydf = mydf.astype({"Sample": 'category'})
 
 
 
@@ -100,3 +100,83 @@ for ii in cols:
                                     )
     print(f'Trait = {ii }   F value = {round(fvalue,3)} p-value = {round(pvalue,3)}')
 
+
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## Repeated Measures within Heat Event
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# REF: https://pandas.pydata.org/pandas-docs/version/0.22/r_interface.html
+# #Exporting a pandas df into R land within python.
+# #A working example
+# from rpy2.robjects import r, pandas2ri
+# import pandas as pd
+# pandas2ri.activate()
+#
+# pd_df = pd.DataFrame({'int_values': [1,2,3],
+#                       'str_values': ['abc', 'def', 'ghi']})
+#
+#
+# r_dataframe = pandas2ri.py2rpy(pd_df)
+# print(r_dataframe)
+
+import rpy2.robjects as robjects
+from rpy2.robjects import pandas2ri
+from rpy2.robjects.packages import importr, data
+robjects.r['options'](warn=-1)  # turn warning messages off
+
+pandas2ri.activate()   # very import step !!!!!
+
+
+# read in core functions
+lme4 =     importr('lme4')
+lmerTest = importr('lmerTest')
+emmeans =  importr('emmeans')
+base = importr('base')
+
+
+r_dataframe = pandas2ri.py2rpy(mydf)
+print(r_dataframe)
+
+print(mydf.columns.values)
+print(mydf.Sample_Date.unique())
+
+res = lme4.lmer('sodium ~ treatment + (1 + treatment|ID)', data = r_dataframe)
+
+#print(res)
+
+# this will run straight R code in R land
+# robjects.r('''
+# library(lme4)
+# library(lmerTest)
+# data(dietox, package='geepack')
+# m<-lmer('Weight ~ Time + (1+Time|Pig)', data=dietox)
+# print(summary(m))
+# ''')
+
+
+
+exit()
+
+
+
+
+
+# base.print(pi)
+#
+# dietox = data(geepack).fetch('dietox')['dietox']
+# base.print(dietox)
+# r.top(dietox)
+#
+# exit()
+
+# from rpy2.robjects.vectors import StrVector
+#
+#
+
+#
+#
+# dietox = data(geepack).fetch('dietox')['dietox']
+#
+# rpy2.robjects.r['dietox']
+#
